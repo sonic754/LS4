c7a7d6e7cc0ca267f55ec1fcdc5bc0951dcc4f03
sonic754 <mfirdavs1402@gmail.com>
2026-01-06 17:23:58 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let son = prompt("moshina nomini kiritnig")
+if(son === "spark"){
+  alert("Chevrolet Spark 2005 yildan 2022 yilgacha General Motors tomonidan ishlab chiqarilgan mashhur ixcham shahar xetchbeki bo‘lib, o‘zining kichik o‘lchamlari, yoqilg‘i tejamkorligi va arzonligi tufayli O‘zbekistonda ayniqsa mashhur edi.");
+}
+else if(son === "matiz"){
+    alert("Chevrolet Matiz - O'zbekistonda shahar ichida harakatlanish uchun eng mashur va ichcham avtobillardan biri bulgan. Modelni ishlab chikarish 2018 yilda twatilgan bwlib, 2025 yilda yangi Matiz avtomobillarini yuklash ikkilamchi bozordan topish mumkin.");
+}
+else if(son === "gentra"){
+    alert("Chevrolet Gentra (O'zbekistonda oddiygina Gentra yoki yangilangan Lacetti deb ataladi) UzAuto Motors zavodida ishlab chiqarilgan chuqur qayta tiklangan Lacetti modelidir. Modelni seriyali ishlab chiqarish nihoyat 2024 yilda to'xtatildi.");
+}
+else if(son === "cobalt"){
+alert("Chevrolet Cobalt 2025-yilda O‘zbekistonda eng ko‘p sotilgan yengil avtomobil bo‘lib qolmoqda. 2025-yil oxirida model 2011-yilda sotuvga chiqarilganidan beri eng keng qamrovli yangilanishdan o‘tkazildi.");
+}
+else if(son === "nexia"){
+alert("Chevrolet Nexia - used in Uzbekistan zhuda mashur bulgan, uzoq tarixga ega avtomobil modeli bwlib, turli avlodlari (Nexia 1, Nexia 2, Nexia 3/Ravon R3) bilan shahar ko'chalarida keng tarkalgan.");
+}
+else if(son === "Lacetti"){
+alert("Chevrolet Lacetti 2003 yildan 2013 yilgacha ishlab chiqarilgan (ba'zi mamlakatlarda, jumladan, O'zbekistonda ham uzoqroq) ixcham C toifali avtomobil bo'lib, italyan dizayn uyi Pininfarina tomonidan o'zining zamonaviy dizayni va byudjetli, ammo yaxshi jihozlangan oilaviy avtomobil sifatida joylashishi bilan mashhur edi.");
+}
+else if(son === "Malibu"){
+alert("Chevrolet Malibu o'rta o'lchamli old g'ildirakli sedan bo'lib, kompaniyaning flagman modeli sifatida joylashtirilgan. U o'ziga xos dizayni va qulay haydash bilan mashhur.");
+}
+else if(son === "BMW"){
+alert("BMW (Bayerische Motoren Werke AG) taniqli nemis ishlab chiqaruvchisi bo'lib, Mini va Rolls-Royce brendlari bilan birga BMW Group tarkibiga kiruvchi premium darajali avtomobillar va mototsikllarni ishlab chiqaradi.");
+}
+else if(son === "Mercedes"){
+alert("Mercedes-Benz jahonga mashhur nemis brendi bo'lib, Mercedes-Benz Group tarkibiga kiradi, u yuqori darajadagi yengil avtomobillar, tijorat transporti va avtobuslar ishlab chiqaradi. Kompaniya avtomobil sanoatida kashshof hisoblanadi va hashamat, innovatsiyalar va muhandislik mukammalligi ramzi hisoblanadi.");
+}
+else if(son === "BWD"){
+alert("BYD (O'z orzularingizni yarating) Xitoyning yirik ko'p millatli konglomerati bo'lib, elektr transport vositalari (EV) va plaginli gibrid avtomobillar (PHEV) ishlab chiqaruvchi yetakchi global ishlab chiqaruvchi hisoblanadi.");
+}
+else{
+    alert("bu moshina tizimda topilmadi")
+}
+
